2293a6b5792671b4a0f187cdb2c134f16937a0bb
guchio33 <aokihikari3@gmail.com>
2024-04-01 01:07:07 +0900

    add:messageModuleの作成

--- backend/app/src/app.module.ts
+++ backend/app/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MessageModule } from './message/message.module';
-import { TaskModule } from './task/task.module';
 import { MessagesModule } from './messages/messages.module';
 
 @Module({
-  imports: [MessageModule, TaskModule, MessagesModule],
+  imports: [MessagesModule],
   controllers: [AppController],
   providers: [AppService],
 })
