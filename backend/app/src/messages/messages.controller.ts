6de5dcb89eeaf81a7f88cb588168469abf002376
guchio33 <aokihikari3@gmail.com>
2024-04-01 01:07:07 +0900

    add:messageModuleの作成

--- backend/app/src/messages/messages.controller.ts
+++ backend/app/src/messages/messages.controller.ts
@@ -1,19 +1,36 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
-import {CreateMessageDto} from './create-message.dto'
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
-  constructor() {}
+  constructor(public messagesService: MessagesService) {}
+
   @Get()
-  listMessages(): string{
-    return "message"
+  listMessages() {
+    return this.messagesService.findAll();
   }
 
   @Post()
-  createMessage(@Body() body: CreateMessageDto) {}
+  createMessage(@Body() body: CreateMessageDto) {
+    return this.messagesService.create(body.content);
+  }
 
   @Get('/:id')
-  getMessage(@Param('id') id: string) {
-    return id
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('message not found');
+    }
+
+    return message;
   }
 }
--- backend/app/src/messages/messages.service.spec.ts
+++ backend/app/src/messages/messages.service.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { TaskService } from './task.service';
+import { MessagesService } from './messages.service';
 
-describe('TaskService', () => {
-  let service: TaskService;
+describe('MessagesService', () => {
+  let service: MessagesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TaskService],
+      providers: [MessagesService],
     }).compile();
 
-    service = module.get<TaskService>(TaskService);
+    service = module.get<MessagesService>(MessagesService);
   });
 
   it('should be defined', () => {
