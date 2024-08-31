3f5179abdd1d7bcfba44fed497e5eeb1f699f81a
guchio33 <aokihikari3@gmail.com>
2024-08-31 13:06:02 +0900

    add:Item更新、Item削除機能の作成

--- backend/app/src/items/items.controller.ts
+++ backend/app/src/items/items.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { ItemsService } from './items.service';
 import { Item } from './item.model';
 import { ItemStatus } from './item-status.model';
@@ -6,8 +14,14 @@ import { ItemStatus } from './item-status.model';
 export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
   @Get()
-  findAll() {
+  findAll(): Item[] {
     return this.itemsService.findAll();
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: string): Item {
+    console.log(id);
+    return this.itemsService.findById(id);
   }
 
   @Post()
@@ -25,5 +39,15 @@ export class ItemsController {
       status: ItemStatus.ON_SALE,
     };
     return this.itemsService.create(item);
+  }
+
+  @Patch(':id')
+  updateStatus(@Param('id') id: string): Item {
+    return this.itemsService.updateStatus(id);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string): void {
+    return this.itemsService.delete(id);
   }
 }
--- backend/app/src/items/items.service.ts
+++ backend/app/src/items/items.service.ts
@@ -1,18 +1,33 @@
 import { Injectable, Get } from '@nestjs/common';
 import { Item } from './item.model';
+import { ItemStatus } from './item-status.model';
 
 @Injectable()
 export class ItemsService {
   private items: Item[] = [];
   @Get()
-  findAll() {
-    return 'hello items';
+  findAll(): Item[] {
+    return this.items;
+  }
+
+  findById(id: string): Item {
+    return this.items.find((item) => item.id === id);
   }
 
   create(item: Item): Item {
-    console.log('item', item);
+    // console.log('item', item);
     this.items.push(item);
-    console.log(this.items);
+    // console.log(this.items);
     return item;
+  }
+
+  updateStatus(id: string): Item {
+    const item = this.findById(id);
+    item.status = ItemStatus.SOLD_OUT;
+    return item;
+  }
+
+  delete(id: string): void {
+    this.items = this.items.filter((item) => item.id !== id);
   }
 }
