8889d06cd4dc0e5dc375dae6428a7c491f7f8712
guchio33 <aokihikari3@gmail.com>
2024-09-01 17:08:00 +0900

    add:パスワードをハッシュ化

--- backend/app/src/auth/auth.repository.ts
+++ backend/app/src/auth/auth.repository.ts
@@ -1,16 +1,21 @@
 import { PrismaClient } from '@prisma/client';
 import { CreateUserDto } from './dto/create_user_dto';
 // import { createCipheriv, randomBytes, scrypt } from 'crypto';
+import * as bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
 
 export class AuthRepository {
   async createUser(createUserDto: CreateUserDto) {
     const { email, password } = createUserDto;
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     return prisma.user.create({
       data: {
-        email,
-        password,
+        email: email,
+        password: hashedPassword,
       },
     });
   }
